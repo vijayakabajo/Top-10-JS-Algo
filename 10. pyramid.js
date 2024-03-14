844c3b6a8dcda60c6bc55644d3dc06ef6819f498
vijayakabajo <vijayakabajo@gmail.com>
2024-03-15 01:09:29 +0530

    completed 10

---
+++ 10. pyramid.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n) {
+    const midpoint = Math.floor((2 * n - 1) / 2);
+
+    for (let row = 0; row < n; row++) {
+        let level = '';
+
+        for (let col = 0; col < 2 * n - 1; col++) {
+            if (midpoint - row <= col && midpoint + row >= col) {
+                level += '#';
+            } else {
+                level += ' ';
+            }
+        }
+
+        console.log(level);
+    }
+}
+
+pyramid(3)
